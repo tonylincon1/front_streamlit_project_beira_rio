2aff4882015ea7acb60624ff132bf6bc911495f2
unknown <antonio.ddramos@gmail.com>
2022-11-02 09:05:11 -0300

    Criando repositório

---
+++ pages/1_👠_Predições.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="IA Calçados Beira Rio",
+    page_icon="👞",
+)
+
+st.title('Uber pickups in NYC')
+
+st.sidebar.selectbox(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary')
+)
+
+st.file_uploader("Choose a file", type=['png', 'jpg'], accept_multiple_files=False)
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
