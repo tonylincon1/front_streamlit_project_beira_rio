ce02abf56925bded242c11a723474c6e7897db14
unknown <antonio.ddramos@gmail.com>
2022-11-04 17:02:27 -0300

    Começando página de predição com escolha da detecção para predição

---
+++ outhers/detect_objet.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import os
+import cv2
+import numpy as np
+import pandas as pd
+#from object_detection.utils import label_map_util
+#from object_detection.utils import visualization_utils as vis_util
+
+# Grab path to current working directory
+CWD_PATH = os.getcwd()
+# Path to frozen detection graph .pb file, which contains the model that is used
+# for object detection.
+PATH_TO_CKPT = 'shoeDetection/Python inference/frozen_inference_graph.pb'
+# Path to label map file
+PATH_TO_LABELS = 'shoeDetection/Python inference/label_map.pbtxt'
+# Number of classes the object detector can identify
+NUM_CLASSES = 1
+
+# Load the label map.
+# Label maps map indices to category names, so that when our convolution
+# network predicts `5`, we know that this corresponds to `king`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+#category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+    sess = tf.compat.v1.Session(graph=detection_graph)
+    
+def trat_detect_objet_extract (input_image):
+
+    # Define input and output tensors (i.e. data) for the object detection classifier
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Load image using OpenCV and
+    # expand image dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+
+    #input_image=cv2.resize(input_image,(500,500))
+    image_expanded = np.expand_dims(input_image, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],feed_dict={image_tensor: image_expanded})
+
+    quant_boxes = len(scores[scores > 0.4])
+    
+    list_images = []
+    contador = 0
+    for box in boxes[0,:quant_boxes]:
+        (height, width) = input_image.shape[:2]
+        ymin = int((boxes[0][contador][0]*height)*0.9)
+        xmin = int((boxes[0][contador][1]*width)*0.9)
+        ymax = int((boxes[0][contador][2]*height)*1.1)
+        xmax = int((boxes[0][contador][3]*width)*1.1)
+        img = np.array(input_image[ymin:ymax,xmin:xmax])
+
+        #Resize
+        img=cv2.resize(img,(500,500))
+        img=img.astype('float32')
+        img=img.reshape(500,500,3)
+        img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        list_images.append(img)
+        contador = contador + 1
+
+    return list_images
+    
+    
--- pages/1_Predições.py
+++ pages/1_Predições.py
@@ -1,38 +1,75 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from outhers.detect_objet import *
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
+
+def criar_subimagem(predict,contador):
+    st.markdown(f"<h6 style='text-align:center'>Essa é a detecão: {contador} <br></h6>", unsafe_allow_html=True)
+    #pilImage = Image.fromarray(predict, "RGB")
+    pilImage = Image.fromarray((predict * 1).astype(np.uint8)).convert('RGB')
+    st.image(pilImage)
 
 st.set_page_config(
-    page_title="IA Calçados Beira Rio",
+    page_title="Predições",
     page_icon="👞",
 )
 
 with open('files/css/styles.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.title('Uber pickups in NYC')
+st.title('Predição de Calçados Semelhantes')
+st.markdown("***")
 
-st.sidebar.selectbox(
-    "What's your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary')
-)
+st.markdown("<h3>Importe aqui a imagem dos seus calçados 👞<br></h3>", unsafe_allow_html=True)
 
-st.file_uploader("Choose a file", type=['png', 'jpg'], accept_multiple_files=False)
+foto_predict = st.file_uploader("Selecione a foto que deseja", type=['png', 'jpg'], accept_multiple_files=False)
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+if foto_predict:
+    st.markdown(f"<h5 style='text-align:center'>Você importou a imagem: {foto_predict.name}<br></h5>", unsafe_allow_html=True)
+    st.image(foto_predict, width=500)
+    foto_predict = np.array(load_image(foto_predict))
+    
+    #Retorno detecção
+    foto_com_detectada = trat_detect_objet_extract(foto_predict)
+    quant_detection = len(foto_com_detectada)
+    st.markdown(f"<h5 style='text-align:left'>Foram detectados {quant_detection} objetos, Esses foram os objetos detectados: <br></h5>", unsafe_allow_html=True)
+    
+    col1,col2 = st.columns(2)
+    col3,col4 = st.columns(2)
+    col5,col6 = st.columns(2)
+    contador = 1
+    
+    for predict in foto_com_detectada:
+        if contador == 1:
+            with col1:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+        elif contador == 2:
+            with col2:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+        elif contador == 3:
+            with col3:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+        elif contador == 4:
+            with col4:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+        elif contador == 5:
+            with col5:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+        elif contador == 6:
+            with col6:
+                criar_subimagem(predict,contador)
+                contador = contador + 1
+    
+    st.selectbox("Qual a imagem que deseja utilizar como referência?",
+                range(1,quant_detection+1))
