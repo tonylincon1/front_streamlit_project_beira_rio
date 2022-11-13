a2a7e9c63251d2a0bfa6db5fc01ee33409d19c1c
unknown <antonio.ddramos@gmail.com>
2022-11-13 01:50:57 -0300

    Corrigindo login

--- Entrar.py
+++ Entrar.py
@@ -14,23 +14,23 @@ st.markdown("***")
 def check_password():
 
     def password_entered():
-        if (st.session_state["username"] in st.secrets["passwords"] and st.session_state["password"] == st.secrets["passwords"][st.session_state["username"]]):
+        if (st.session_state["db_username"] in st.secrets["db_username"] and st.session_state["db_password"] == st.secrets["db_password"][st.session_state["db_username"]]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+            del st.session_state["db_username"]
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
         # First run, show inputs for username + password.
-        st.text_input("Usuário", key="username")
+        st.text_input("Usuário", key="db_username")
         st.text_input("Senha", type="password", key="password")
         if st.button("Entrar"):
             password_entered()
             return False
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
-        st.text_input("Usuário", key="username")
+        st.text_input("Usuário", key="db_username")
         st.text_input("Senha", type="password", key="password")
         if st.button("Entrar"):
             password_entered()
